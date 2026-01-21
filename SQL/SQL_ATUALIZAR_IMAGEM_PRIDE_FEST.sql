987ed662ffb6282a6c736ac013e89e7547cb2d2a
Juliana Daveiga <juliana.daveiga@example.com>
2026-01-21 11:42:19 -0300

    feat: ajustar qualidade da imagem do Pride Fest (w=1200)

--- SQL/SQL_ATUALIZAR_IMAGEM_PRIDE_FEST.sql
+++ SQL/SQL_ATUALIZAR_IMAGEM_PRIDE_FEST.sql
@@ -2,7 +2,7 @@
 -- Foto: Bandeira do arco-íris sendo carregada por multidão em parada
 
 UPDATE events
-SET image = 'https://images.unsplash.com/photo-1558618666-fcd25c85cd64?w=800&q=80'
+SET image = 'https://images.unsplash.com/photo-1558618666-fcd25c85cd64?w=1200&q=80'
 WHERE name ILIKE '%Pride Fest%' 
    OR name ILIKE '%Festival LGBTQIA%'
    OR (name ILIKE '%Pride%' AND name ILIKE '%2025%');
